fd37c0aff31afff09738aa54cc689a6d905cacb9
prof-pccarvalho <pcbarroso@gmail.com>
2022-03-22 10:54:15 -0300

    Initial commit

---
+++ src/main/java/Util/BankTest.java
@@ -0,0 +1,13 @@
+package Util;
+
+import br.com.leba.Bank.Bank;
+
+public class BankTest {
+
+    public static void main(String args[]) {
+        Bank myBank = new Bank();
+        myBank.name = "Banco do Brasil";
+        System.out.println(myBank.name);
+    }
+
+}
---
+++ src/main/java/br/com/leba/Bank/Bank.java
@@ -0,0 +1,6 @@
+package br.com.leba.Bank;
+
+public class Bank {
+    public String name;
+    Client clients[];
+}
